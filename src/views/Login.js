7ed8e935cd0176561f42250328fc3341ef3f3f58
Mohamed Irfan <irfancuber@gmail.com>
2021-02-20 19:42:21 +0800

    Added Login form component in Login.js view. Used react-bootstrap to create. Also added styles for the login component in src/styles/Login.css.

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+import "../styles/Login.css";
 
-class Login extends React.Component {
-    render(){
-        return(
-            <h1>Login</h1>
-        );
-    }
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <div className="Login">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group size="lg" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        {/* Hard Coded for now, need to remove! */}
+        <Link to='/blog-overview'>
+          <Button block size="lg" type="submit" disabled={!validateForm()}>
+            Login
+          </Button>
+        </Link>
+      </Form>
+    </div>
+  );
 }
-
-export default Login;
